7a2a717f69543a39b26100fceed6b758eb138009
Nikolai Makarevich <nikoladze529@mail.ru>
2026-09-22 23:30:10 +0300

    init: init slider implementation

---
+++ src/scripts/slider.js
@@ -0,0 +1,14 @@
+let currentSlide = 1;
+const prevSlideButton = document.querySelector('.slider_control.prev');
+const nextSlideButton = document.querySelector('.slider_control.next');
+const sliderMarkers = document.querySelector('.slider-markers');
+const slidesList = document.querySelector('.slides-list');
+
+function prevSlide() {}
+
+function nextSlide() {}
+
+function selectSlide(slideIndex) {}
+
+prevSlideButton.addEventListener('click', prevSlide);
+nextSlideButton.addEventListener('click', nextSlide);
