30eb2448cfd9933e6cd9b02934d41d09f66f48d7
Nikolai Makarevich <nikoladze529@mail.ru>
2026-09-24 00:48:18 +0300

    refactor: update burger menu according task requirements

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -2,22 +2,57 @@ let isBurgerOpen = false;
 const burger = document.querySelector('.burger-menu');
 const nav = document.querySelector('.header-nav');
 
+function closeBurger() {
+  isBurgerOpen = false;
+  burger.classList.remove('open');
+  nav.classList.remove('open');
+  enableScroll();
+}
+
+function handleScroll(event) {
+  event.preventDefault();
+}
+
+function disableScroll() {
+  document.addEventListener('scroll', handleScroll, { passive: false });
+  document.addEventListener('wheel', handleScroll, { passive: false });
+  document.addEventListener('touchmove', handleScroll, { passive: false });
+}
+
+function enableScroll() {
+  document.removeEventListener('wheel', handleScroll);
+  document.removeEventListener('scroll', handleScroll);
+  document.removeEventListener('touchmove', handleScroll);
+}
+
 burger.addEventListener('click', () => {
   isBurgerOpen = !isBurgerOpen;
   if (isBurgerOpen) {
+    nav.classList.add('transition');
     burger.classList.add('open');
     nav.classList.add('open');
+    disableScroll();
   } else {
-    burger.classList.remove('open');
-    nav.classList.remove('open');
+    closeBurger();
   }
 });
 
 nav.addEventListener('click', (event) => {
   const { target } = event;
   if (target.tagName === 'A') {
-    isBurgerOpen = false;
-    burger.classList.remove('open');
-    nav.classList.remove('open');
+    closeBurger();
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'Escape') {
+    closeBurger();
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (document.body.offsetWidth >= 769) {
+    nav.classList.remove('transition');
+    closeBurger();
   }
 });
