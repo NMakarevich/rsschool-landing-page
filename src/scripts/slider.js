b44cd7922a76ca79c529c2ef2e9cf0ae086bb42b
NMakarevich <nikoladze529@mail.ru>
2026-09-23 15:48:57 +0300

    feat: add slide function

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,3 +1,5 @@
+import items from '../data/slides.json';
+
 let currentSlide = 1;
 const prevSlideButton = document.querySelector('.slider_control.prev');
 const nextSlideButton = document.querySelector('.slider_control.next');
@@ -12,3 +14,19 @@ function selectSlide(slideIndex) {}
 
 prevSlideButton.addEventListener('click', prevSlide);
 nextSlideButton.addEventListener('click', nextSlide);
+
+function getSlideItem(item) {
+  const element = document.createElement('li');
+  element.classList.add('slides-list_item');
+  element.innerHTML = `<div class="slider-item">
+                        <div class="slider-item_image image-wrapper">
+                          <img src="./src/assets/images/slider/${item.image}" alt="item.name">
+                        </div>
+                        <div class="slider-item_info">
+                          <h3 class="slider-item_title">${item.name}</h3>
+                          <p class="slider-item_description">${item.description}</p>
+                          <h3 class="slider-item_price">${item.price}</h3>
+                        </div>
+                      </div>`;
+  return element;
+}
