c67bde1145e5ec1b3cb88c42fb42fc19438ae48e
NMakarevich <nikoladze529@mail.ru>
2026-09-25 15:57:22 +0300

    refactor: rewrite code to class

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,8 +1,38 @@
 import { createElement, disableScroll, enableScroll } from './utils.js';
 
-function createModal(item) {
-  const modal = createElement('div', 'modal');
-  modal.innerHTML = `<div class="modal-overlay"></div>
+export class Modal {
+  overlay = null;
+  closeButton = null;
+  modal = null;
+
+  constructor() {}
+
+  createToggleGroupItem(iconValue, value, isActive = false) {
+    return `<li class="item-toggle-group_item">
+            <button type="button" class="button tab-control ${isActive ? 'active' : ''}" data-value="${iconValue}">
+              <span class="tab-control_icon">${typeof iconValue === 'string' ? iconValue.toUpperCase() : iconValue}</span> ${value}
+            </button>
+          </li>`;
+  }
+
+  getAdditivesItems(items) {
+    return items.reduce(
+      (acc, item, index) => acc + this.createToggleGroupItem(index + 1, item.name),
+      ''
+    );
+  }
+
+  getSizeItems(sizes) {
+    return Object.entries(sizes).reduce(
+      (acc, [size, value], index) =>
+        acc + this.createToggleGroupItem(size, value.size, index === 0),
+      ''
+    );
+  }
+
+  createModal(item) {
+    const modal = createElement('div', 'modal');
+    modal.innerHTML = `<div class="modal-overlay"></div>
         <div class="item">
           <div class="image-wrapper">
             <img class="item-image" src="./src/assets/images/${item.category}/${item.image}" alt="${item.name}">
@@ -14,11 +44,11 @@ function createModal(item) {
             </div>
             <div class="item-toggle-group size">
               <span class="item-toggle-group_label">Size</span>
-              <ul class="item-toggle-group_list">${getSizeItems(item.sizes)}</ul>
+              <ul class="item-toggle-group_list">${this.getSizeItems(item.sizes)}</ul>
             </div>
             <div class="item-toggle-group additives">
               <span class="item-toggle-group_label">Additives</span>
-              <ul class="item-toggle-group_list">${getAdditivesItems(item.additives)}</ul>
+              <ul class="item-toggle-group_list">${this.getAdditivesItems(item.additives)}</ul>
             </div>
             <div class="item-total-price">
               <h3 class="item-total-price_title">Total:</h3>
@@ -42,43 +72,32 @@ function createModal(item) {
             <button type="button" class="button button-secondary modal-close">Close</button>
           </div>
         </div>`;
-  return modal;
-}
-
-function createToggleGroupItem(iconValue, value, isActive = false) {
-  return `<li class="item-toggle-group_item">
-            <button type="button" class="button tab-control ${isActive ? 'active' : ''}" data-value="${iconValue}">
-              <span class="tab-control_icon">${typeof iconValue === 'string' ? iconValue.toUpperCase() : iconValue}</span> ${value}
-            </button>
-          </li>`;
-}
-
-function getSizeItems(sizes) {
-  return Object.entries(sizes).reduce(
-    (acc, [size, value], index) => acc + createToggleGroupItem(size, value.size, index === 0),
-    ''
-  );
-}
-
-function getAdditivesItems(items) {
-  return items.reduce((acc, item, index) => acc + createToggleGroupItem(index + 1, item.name), '');
-}
-
-function openModal(item) {
-  const modal = createModal(item);
-  document.body.appendChild(modal);
-  disableScroll();
-  const overlay = modal.querySelector('.modal-overlay');
-  const closeButton = modal.querySelector('.modal-close');
-
-  function closeModal(event) {
-    if ((event.type === 'keyup' && event.key === 'Escape') || event.type === 'click') {
-      enableScroll();
-      modal.remove();
-    }
+    return modal;
   }
 
-  overlay.addEventListener('click', closeModal);
-  closeButton.addEventListener('click', closeModal);
-  document.addEventListener('keyup', closeModal);
+  closeModal = (event) => {
+    if ((event.type === 'keyup' && event.key === 'Escape') || event.type === 'click') {
+      enableScroll();
+      this.modal.remove();
+      this.modal = null;
+      this.closeButton = null;
+      this.overlay = null;
+    }
+  };
+
+  eventListeners = () => {
+    if (this.overlay) this.overlay.addEventListener('click', this.closeModal);
+    if (this.closeButton) this.closeButton.addEventListener('click', this.closeModal);
+    document.addEventListener('keyup', this.closeModal);
+  };
+
+  openModal(item) {
+    this.modal = this.createModal(item);
+    document.body.appendChild(this.modal);
+    disableScroll();
+    this.overlay = this.modal.querySelector('.modal-overlay');
+    this.closeButton = this.modal.querySelector('.modal-close');
+
+    this.eventListeners();
+  }
 }
