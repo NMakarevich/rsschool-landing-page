deadec8600bceb87f25edc34f41f879f93fbb0f8
Nikolai Makarevich <nikoladze529@mail.ru>
2026-09-26 00:26:02 +0300

    feat: add hiding loadMore button if category size less or equal min value

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -2,6 +2,8 @@ import products from '../data/products.json';
 import { MenuItem } from './menuItem.js';
 import { Modal } from './modal.js';
 
+const MIN_CATEGORY_SIZE_FOR_LOAD_MORE = 5;
+
 const modal = new Modal();
 const categories = products.reduce((acc, product) => {
   if (acc[product['category']]) {
@@ -16,6 +18,17 @@ let selectedCategory = Object.keys(categories)[0];
 
 const tabControls = document.querySelector('.tabs-controls');
 const menuList = document.querySelector('.menu-list');
+const loadMoreButton = document.querySelector('.load-more');
+
+function setLoadMoreButtonStyle() {
+  if (categories[selectedCategory].length < MIN_CATEGORY_SIZE_FOR_LOAD_MORE) {
+    loadMoreButton.style.display = 'none';
+  } else {
+    loadMoreButton.style = '';
+  }
+}
+
+setLoadMoreButtonStyle();
 
 categories[selectedCategory].forEach((item) => new MenuItem(item, modal).render());
 
@@ -30,12 +43,12 @@ function changeCategory(event) {
 
   menuList.classList.remove('show-all');
   menuList.innerHTML = '';
+  setLoadMoreButtonStyle();
   categories[selectedCategory].forEach((item) => new MenuItem(item, modal).render());
 }
 
 tabControls.addEventListener('click', changeCategory);
 
-const loadMoreButton = document.querySelector('.load-more');
 loadMoreButton.addEventListener('click', () => {
   menuList.classList.add('show-all');
 });
