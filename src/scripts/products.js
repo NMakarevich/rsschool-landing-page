511893081589b96cfb1abab7e73db76882aacf39
NMakarevich <nikoladze529@mail.ru>
2026-09-25 15:58:41 +0300

    feat: add product script

---
+++ src/scripts/products.js
@@ -0,0 +1,38 @@
+import products from '../data/products.json';
+import { MenuItem } from './menuItem.js';
+import { Modal } from './modal.js';
+
+const modal = new Modal();
+const categories = {};
+
+for (let product of products) {
+  if (categories[product.category]) {
+    categories[product.category].push(product);
+  } else {
+    categories[product.category] = [product];
+  }
+}
+
+let selectedCategory = Object.keys(categories)[0];
+
+const tabControls = document.querySelector('.tabs-controls');
+const menuList = document.querySelector('.menu-list');
+
+menuList.append(...categories[selectedCategory].map((item) => new MenuItem(item, modal).render()));
+
+function changeCategory(event) {
+  const { target } = event;
+
+  if (target.tagName !== 'BUTTON') return;
+
+  document.querySelector(`[data-category="${selectedCategory}"]`).classList.remove('active');
+  selectedCategory = target.dataset.category;
+  target.classList.add('active');
+
+  menuList.innerHTML = '';
+  menuList.append(
+    ...categories[selectedCategory].map((item) => new MenuItem(item, modal).render())
+  );
+}
+
+tabControls.addEventListener('click', changeCategory);
